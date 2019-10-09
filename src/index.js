e7a631fecbd216cc7c6748cf3b7c2ef921fa42e2
sunaryaagung <sunaryaagung95@gmail.com>
2019-10-10 04:17:13 +0700

    add env

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
-console.log("SUPERMAN", process.env.REACT_APP_NAME);
+console.log("SUPERMAN", process.env.REACT_APP_API_URL);
 
 ReactDOM.render(
   <BrowserRouter>
